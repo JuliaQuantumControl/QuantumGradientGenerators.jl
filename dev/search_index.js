593f96af6b9c5ac0c1232ba25e524231dcd66e3c
Documenter.jl <documenter@juliadocs.github.io>
2022-12-22 03:17:20 +0000

    build based on 03ba665

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuantumGradientGenerators","category":"page"},{"location":"#QuantumGradientGenerators","page":"Home","title":"QuantumGradientGenerators","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuantumGradientGenerators.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuantumGradientGenerators]","category":"page"}]
+}
