2f9ab044bc3059c09e7086c562f6d1526ad381a5
Michael Goerz <mail@michaelgoerz.net>
2023-02-15 13:58:19 -0500

    Delete "stable" (should be regenerated with next release)

--- stable/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuantumGradientGenerators","category":"page"},{"location":"#QuantumGradientGenerators","page":"Home","title":"QuantumGradientGenerators","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuantumGradientGenerators.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuantumGradientGenerators]","category":"page"}]
-}
